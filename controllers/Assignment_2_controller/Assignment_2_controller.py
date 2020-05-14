28df00438db634b7adbc7cfdb7635777e3d1d3f4
Daniel Whiteman <danielwhiteman718@gmail.com>
2020-05-14 17:49:57 +0100

    Connecting camera to the controller and making the proto

--- controllers/Assignment_2_controller/Assignment_2_controller.py
+++ controllers/Assignment_2_controller/Assignment_2_controller.py
@@ -23,6 +23,9 @@ motorRight.setVelocity(0.0)
 ds = robot.getLidar('lidar')
 ds.enable(timestep)
 
+cam = robot.getCamera('camera')
+cam.enable(timestep)
+
 motorLeft.setVelocity(6.3)
 motorRight.setVelocity(6.3)
 # Main loop:
