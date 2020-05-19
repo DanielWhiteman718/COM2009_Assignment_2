13710c0986924a1b5a296fca935067585ff37cd2
unknown <mjarmocik@gmail.com>
2020-05-19 02:36:30 +0100

    lidar example

--- controllers/Assignment_2_controller/Assignment_2_controller.py
+++ controllers/Assignment_2_controller/Assignment_2_controller.py
@@ -1,17 +1,12 @@
-"""my_controller controller."""
-
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
+"""Assignment controller."""
 from controller import Robot, DistanceSensor, Motor, Lidar
 
-# create the Robot instance.
 robot = Robot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
+#setup motors
 motorLeft = robot.getMotor('wheel_left')
 motorRight = robot.getMotor('wheel_right')
 
@@ -20,23 +15,24 @@ motorRight.setPosition(float('inf'))
 motorLeft.setVelocity(0.0)
 motorRight.setVelocity(0.0)
 
+#setup sensors
 ds = robot.getLidar('lidar')
 ds.enable(timestep)
 cam = robot.getCamera('camera')
 cam.enable(timestep)
-motorLeft.setVelocity(6.3)
-motorRight.setVelocity(6.3)
+
+#run motors
+motorLeft.setVelocity(2)
+motorRight.setVelocity(2)
+
 # Main loop:
-# - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
-
-# Enter here exit cleanup code.
+    front = ds.getRangeImageArray()[255][2]
+    left = ds.getRangeImageArray()[0][2]
+    right = ds.getRangeImageArray()[511][2]
+    
+    front = "{:.2f}".format(front)
+    left = "{:.2f}".format(left)
+    right = "{:.2f}".format(right)
+    
+    print(front + " | " + left + " | " + right + "      Front | Left | Right")
