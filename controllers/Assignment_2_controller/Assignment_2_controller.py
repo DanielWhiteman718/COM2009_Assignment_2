2d7e9062fd80262cc9c46b65313b83c48d618d34
unknown <mjarmocik@gmail.com>
2020-05-21 00:09:24 +0100

    Changed robot motor naming, robot physics (wheels and body wieght), made the robot shorter. Controller now can follow wall to the right

--- controllers/Assignment_2_controller/Assignment_2_controller.py
+++ controllers/Assignment_2_controller/Assignment_2_controller.py
@@ -1,5 +1,17 @@
 """Assignment controller."""
 from controller import Robot, DistanceSensor, Motor, Lidar
+import math
+
+def stopMotors():
+    motorLeft.setVelocity(0)
+    motorRight.setVelocity(0)
+    
+def turn_90(direction):
+    motorRight.setPosition(1)
+    motorLeft.setPosition(-1)
+
+    #motorRight.setVelocity(-1)
+    #motorLeft.setVelocity(1)
 
 robot = Robot()
 
@@ -21,18 +33,76 @@ ds.enable(timestep)
 cam = robot.getCamera('camera')
 cam.enable(timestep)
 
-#run motors
-motorLeft.setVelocity(2)
-motorRight.setVelocity(2)
+start = True
+mode = 'straight'
+mode_start = robot.getTime()
+start_time = robot.getTime()
 
 # Main loop:
 while robot.step(timestep) != -1:
-    front = ds.getRangeImageArray()[255][2]
-    left = ds.getRangeImageArray()[0][2]
-    right = ds.getRangeImageArray()[511][2]
+    if robot.getTime() - 2 < start_time:
+        continue
     
-    front = "{:.2f}".format(front)
-    left = "{:.2f}".format(left)
-    right = "{:.2f}".format(right)
+    if start:
+        #run motors
+        motorLeft.setVelocity(2)
+        motorRight.setVelocity(2)
+        start = False
+        
+    # lidar data
+    rangeImage = ds.getRangeImageArray()
     
-    print(front + " | " + left + " | " + right + "      Front | Left | Right")
+    # lidar debuging
+    #front = rangeImage[89][2]
+    #left = rangeImage[0][2]
+    #right = rangeImage[179][2]
+    #front = "{:.2f}".format(front)
+    #left = "{:.2f}".format(left)
+    #right = "{:.2f}".format(right)
+    #print(front + " | " + left + " | " + right + "      Front | Left | Right")
+    
+    lidar_0 = rangeImage[179][2]   # distance to right 
+    lidar_30 = rangeImage[149][2]  # distance to right minus 30 degrees
+    front = rangeImage[90][2]
+    
+    ratio = round(lidar_30 / lidar_0, 2)
+    
+    # turn left if wall dead ahead
+    if front < 0.22:
+        if mode != 'left':
+            print('Turning left')
+            mode = 'left'
+            
+        motorLeft.setVelocity(0.5)
+        motorRight.setVelocity(3)
+        mode_start = robot.getTime()
+    
+    # going parallel to the wall
+    if ratio >= 1.1 and ratio <= 1.2 and robot.getTime() - 0.2 > mode_start:
+        if mode != 'straight':
+            print('Going straight')
+            mode = 'straight'
+            
+        motorLeft.setVelocity(2)
+        motorRight.setVelocity(2)
+        mode_start = robot.getTime()
+    
+    # need to turn right    
+    elif ratio > 1.2 and robot.getTime() - 0.2 > mode_start:
+        if mode != 'right':
+            print('Turning right')
+            mode = 'right'
+            
+        motorLeft.setVelocity(3)
+        motorRight.setVelocity(0.5)
+        mode_start = robot.getTime()
+    
+    # need to turn left
+    elif ratio < 1.1 and robot.getTime() - 0.2 > mode_start:
+        if mode != 'left':
+            print('Turning left')
+            mode = 'left'
+            
+        motorLeft.setVelocity(0.5)
+        motorRight.setVelocity(3)
+        mode_start = robot.getTime()
